94e78d4b7634f655b01019bba025fa7747e781d2
Milly Rodriguez <millz118@gmail.com>
2018-10-03 14:42:55 -0400

    added read and update function

--- ProductsApp/routes/product.js
+++ ProductsApp/routes/product.js
@@ -9,4 +9,9 @@ router.get('/test', product_controller.test);
 
 router.post('/create', product_controller.product_create);
 
+router.get('/:id', product_controller.product_details);
+
+router.put('/:id/update', product_controller.product_update);
+
+
 module.exports = router;
