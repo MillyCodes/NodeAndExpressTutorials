687275ab69e5a4f14ae1d626581638f27163fe42
Milly Rodriguez <millz118@gmail.com>
2018-10-07 23:02:18 -0400

    completed crud routes on  notes app

--- notable/app/routes/note_routes.js
+++ notable/app/routes/note_routes.js
@@ -1,6 +1,56 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+    //CREATE => make a new note => POST request
     app.post('/notes', (req, res) => {
-        // You'll create your note here.
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
+
+    //READ => get a specific note => GET request
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details,(err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    //UPDATE => change a current note => PUT request
+    app.get('notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send(note);
+            }
+        })
+    });
+
+    //DELETE => permanently remove specific note 
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            } else {
+                res.send('Note ' + id + ' deleted');
+            }
+        });
+    });
+
 };
--- notable/server.js
+++ notable/server.js
@@ -1,13 +1,22 @@
 // server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app            = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-    console.log('We are live on ' + port);
-});
+app.use(bodyParser.urlencoded({ extended: true}));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    // add the database name and not the collection name. Research for MongoDB (3.0+)
+    // db = database.db("notesapp")
+    require('./app/routes')(app, database);
+
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
